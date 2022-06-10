c65c91090fb6233108547ba3528addfdf0f40be4
athavan <52910050+sundaraathavan@users.noreply.github.com>
2022-06-09 20:59:22 -0700

    master

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
+
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Athavan it's my first react native</Text>
-      <StatusBar style="auto" />
+      <Text>Athavan it's my first react native app</Text>
+      
     </View>
   );
 }
